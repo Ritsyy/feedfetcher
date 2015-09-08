e37ad1e19c8409a3dee54a913e1183c7fd35b6e4
ritsyy <richarupela95@gmail.com>
2015-09-08 17:14:03 +0530

    All urls Resolved

--- rssfeedfetcher/feeds.py
+++ rssfeedfetcher/feeds.py
@@ -6,8 +6,12 @@ def feeddata(url):
 	page = urllib2.urlopen(url)
 	soup = BeautifulSoup(page)
 	link = soup.find('link', type='application/rss+xml')
-	if link:
-		feed = feedparser.parse( link['href'] )
+	finalurl = link['href']
+	if finalurl.startswith("/"):
+		finalurl = url + link['href']
+	print finalurl
+	if finalurl:
+		feed = feedparser.parse( finalurl )
 	else:
 		feed = feedparser.parse( url )
 	# print feed["items"]
